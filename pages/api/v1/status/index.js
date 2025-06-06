523a1c55fa72007d23b319417c84018f4885a3c9
Rubens <rubenspradosilva@gmail.com>
2025-06-05 23:13:18 -0300

    add `postgres` e `.env `

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, resoponse) {
+import database from "../../../../infra/database.js";
+
+async function status(request, resoponse) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   resoponse.status(200).json({ chave: "valor" });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
-test("GET to /api/v1/status should 200", async () => {
+test("GET to api/v1/status should return 200", async () => {
   const resoponse = await fetch("http://localhost:3000/api/v1/status");
   expect(resoponse.status).toBe(200);
 });
